db06f0d93accac781f3daab2daa3beea2d7447ba
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2021-05-29 15:12:39 +0530

    Initial commit

---
+++ html/index.php
@@ -0,0 +1,474 @@
+<?php include 'sendemail.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="keywords" content="WebPaage Technologies,  Build your dream online website with us. Create a professional platform without any complicated struggle. We are delivering success with every solution. Contact us for Web Designing and Development, SEO or (Search Engine Optimisation), and Applications Development. and Support Indian Developers from Bareilly, Uttar Pradesh , India!!">
+   
+  <meta name="description" content="Web Designing and Development, SEO or (Search Engine Optimisation), and Applications Development. and Support Indian Developers from Bareilly, Uttar Pradesh , India!!">
+
+  <meta name="copyright" content="WebPaage , https://www.WebPaage.in/">
+
+  <title>WebPaage Technologies | Web Designing And Development | SEO (Search engine optimization) | Application Development Services in Bareilly </title>
+
+  <link rel="shortcut icon" href="../assets/favicon.png" type="image/x-icon">
+
+  <link rel="stylesheet" href="../assets/css/maicons.css">
+
+  <link rel="stylesheet" href="../assets/vendor/animate/animate.css">
+
+  <link rel="stylesheet" href="../assets/vendor/owl-carousel/css/owl.carousel.min.css">
+
+  <link rel="stylesheet" href="../assets/css/bootstrap.css">
+
+  <link rel="stylesheet" href="../assets/css/mobster.css">
+
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light navbar-floating nav-dark">
+  <div class="container">
+    <a class="navbar-brand" href="index.php">
+      <img src="../assets/img/logo.png" alt="" width="80" height="80">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  
+    <div class="collapse navbar-collapse" id="navbarToggler">
+      <ul class="navbar-nav ml-lg-5 mt-3 mt-lg-0">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="about.html">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="updates.html">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contact.php">Contact</a>
+        </li>
+      </ul>
+      <div class="ml-auto my-2 my-lg-0">
+        <a class="nav-link" href="contact.php"><button class="btn btn-dark rounded-pill">Contact NOW</button></a>
+      </div>
+    </div>
+  </div>
+</nav>
+  
+<div class="page-hero-section bg-image hero-home-1" style="background-image: url(../assets/img/bg_hero_1.svg);">
+
+  <div class="hero-caption pt-5">
+    <div class="container h-100">
+      <div class="row align-items-center h-100">
+        <div class="col-lg-6 wow fadeInUp" style="font-family: serif; ">
+        	<br>
+        	<br>
+        	<br>
+          <div class="badge mb-2"><span class="icon mr-1"><span class="mai-globe"></span></span> We Are Always Ready To Help You</div>
+          <h1 class="mb-4">Grow Your Business Online With Us</h1>
+          <p class="mb-4">When everything is online then why are'nt you? Take your buisness online and reach the people all over the world not just in Bareilly or UP.
+Create your professional looking website design today. Or already have a website looking to take it one step ahead? We will develop your website with the look you imagined.
+<br>
+
+Just Contact Us!! Support local developers from Bareilly.
+<br>
+Take a tour of our website and browse through amazing web development plans today!
+</p>
+        </div>
+        <div class="col-lg-6 d-none d-lg-block wow zoomIn">
+          <div class="img-place mobile-preview shadow floating-animate">
+            <img src="../assets/img/app_preview_1.png"  alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Clients -->
+<div class="page-section mt-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6 col-lg-3 py-3 wow zoomIn">
+        <div class="img-place client-img">
+          <img src="../assets/img/clients/p1.png" alt="">
+        </div>
+      </div>
+      <div class="col-sm-6 col-lg-3 py-3 wow zoomIn">
+        <div class="img-place client-img">
+          <img src="../assets/img/clients/p5.jpg" alt="">
+        </div>
+      </div>
+      <div class="col-sm-6 col-lg-3 py-3 wow zoomIn">
+        <div class="img-place client-img">
+          <img src="../assets/img/clients/akshat.jpg" alt="">
+        </div>
+      </div>
+      <div class="col-sm-6 col-lg-3 py-3 wow zoomIn">
+        <div class="img-place client-img">
+          <img src="../assets/img/clients/p4.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</div> <!-- End clients -->
+
+<div class="position-realive bg-image" style="background-image: url(../assets/img/pattern_1.svg);">
+
+<div class="page-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-5 py-3">
+        <div class="img-place mobile-preview shadow wow zoomIn">
+          <img src="../assets/img/app_preview_2.png" alt="">
+        </div>
+      </div>
+      <div class="col-lg-6 py-3 mt-lg-5">
+        <div class="iconic-list">
+          <div class="iconic-item wow fadeInUp">
+            <div class="iconic-md iconic-text bg-warning fg-white rounded-circle">
+              <span class="mai-cube"></span>
+            </div>
+            <div class="iconic-content">
+              <h5>Best Web Hosting</h5>
+              <p class="fs-small">A web hosting service is a type of Internet hosting service that allows individuals and organizations to make their website accessible via the World Wide Web</p>
+            </div>
+          </div>
+          <div class="iconic-item wow fadeInUp">
+            <div class="iconic-md iconic-text bg-info fg-white rounded-circle">
+              <span class="mai-shield"></span>
+            </div>
+            <div class="iconic-content">
+              <h5>Fully Secured</h5>
+              <p class="fs-small">We can’t forget about the statistics, which make website security a compelling topic for any online business—regardless of their size.</p>
+            </div>
+          </div>
+          <div class="iconic-item wow fadeInUp">
+            <div class="iconic-md iconic-text bg-indigo fg-white rounded-circle">
+              <span class="mai-desktop-outline"></span>
+            </div>
+            <div class="iconic-content">
+              <h5>Easy Monitoring</h5>
+              <p class="fs-small">Monitoring is essential to ensure that a website is available to users</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="page-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6 offset-lg-1 py-3 mt-lg-5 wow fadeInUp" style="font-family: serif;">
+        <h1 class="mb-4">Ecommerce business opperate easilly</h1>
+        <p class="mb-4">Why aren't your shop Website is not online yet?
+In this hard times every commercial buisness took hit. And hit real hard! But some buisness were not at all at loss instead they made huge profit. Why? Because if you open your 
+buisness in formal timings for someone to visit your store. It's asking too much at current scenerio.
+Solution is begin your E-Commerce Buisness Now. Give round of clock time to your customers to reach you. Whenever they want, from wherever they are. 
+It is a one-time investment to expand your buisness to huge amount of new customers. And doing this, support your buisness and local developers from Bareilly.
+Look at our flexible plans today at WebPaage.in </p>
+        <a href="updates.html" class="btn btn-outline-primary rounded-pill">Services</a>
+      </div>
+      <div class="col-lg-5 py-3">
+        <div class="img-place mobile-preview shadow wow zoomIn">
+          <img src="../assets/img/app_preview_3.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+</div>
+
+
+<div class="page-section bg-dark fg-white">
+  <div class="container">
+    <h1 class="text-center">Why Choose Us</h1>
+
+    <div class="row justify-content-center mt-5">
+      <div class="col-md-6 col-lg-3 py-3">
+        <div class="card card-body border-0 bg-transparent text-center wow zoomIn">
+          <div class="mb-3">
+            <img src="../assets/img/icons/rocket.svg" alt="">
+          </div>
+          <p class="fs-large">Very Fast</p>
+         
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-3 py-3">
+        <div class="card card-body border-0 bg-transparent text-center wow zoomIn" data-wow-delay="200ms">
+          <div class="mb-3">
+            <img src="../assets/img/icons/testimony.svg" alt="">
+          </div>
+          <p class="fs-large">Happy Client</p>
+          
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-3 py-3">
+        <div class="card card-body border-0 bg-transparent text-center wow zoomIn" data-wow-delay="400ms">
+          <div class="mb-3">
+            <img src="../assets/img/icons/promotion.svg" alt="">
+          </div>
+          <p class="fs-large">Free Ads</p>
+         
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-3 py-3">
+        <div class="card card-body border-0 bg-transparent text-center wow zoomIn" data-wow-delay="600ms">
+          <div class="mb-3">
+            <img src="../assets/img/icons/coins.svg" alt="">
+          </div>
+          <p class="fs-large">Save Money</p>
+        
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-3 py-3">
+        <div class="card card-body border-0 bg-transparent text-center wow zoomIn" data-wow-delay="800ms">
+          <div class="mb-3">
+            <img src="../assets/img/icons/support.svg" alt="">
+          </div>
+          <p class="fs-large">24/7 Support</p>
+   
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-3 py-3">
+        <div class="card card-body border-0 bg-transparent text-center wow zoomIn" data-wow-delay="1000ms">
+          <div class="mb-3">
+            <img src="../assets/img/icons/laptop.svg" alt="">
+          </div>
+          <p class="fs-large">Full Features</p>
+         
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="page-section bg-image" style="background-image: url(../assets/img/pattern_2.svg);">
+  <div class="container">
+    <div class="row justify-content-center align-items-center">
+      <div class="col-lg-5 mb-5 mb-lg-0 wow fadeInUp">
+        <h1 class="mb-4">Pricing Plan</h1>
+        <p class="mb-5">The Good-Better-Best Approach to Pricing</p>
+        <a href="contact.php" class="btn btn-gradient btn-split-icon rounded-pill">
+          <span class="icon mai-call-outline"></span> Custom Plan
+        </a>
+      </div>
+      <div class="col-lg-7">
+        <div class="pricing-table">
+          <div class="pricing-item active wow zoomIn">
+            <div class="pricing-header">
+              <h5>Static Plan</h5>
+              <br>
+              <h1 class="fw-normal" style="font-size: 35px;">4999-9999/-</h1>
+            </div>
+            <div class="pricing-body">
+              <ul class="theme-list">
+                <li class="list-item">View Level </li>
+                <li class="list-item">Unlimited Bandwith</li>
+                <li class="list-item">Realtime Database</li>
+                <li class="list-item">Monthly Backup</li>
+
+                <li class="list-item">24/7 Support</li>
+              </ul>
+            </div>
+            <button class="btn btn-dark">Choose Plan</button>
+          </div>
+          <div class="pricing-item wow zoomIn" data-wow-delay="200ms">
+            <div class="pricing-header">
+              <h5>Dynamic Plan</h5>
+              <br>
+              <h1 class="fw-normal" style="font-size: 35px;">14999-24999/-</h1>
+            </div>
+            <div class="pricing-body">
+              <ul class="theme-list">
+                <li class="list-item">E-commerce</li>
+                <li class="list-item">Unlimited Bandwith</li>
+                <li class="list-item">Realtime Database</li>
+                <li class="list-item">Monthly Backup</li>
+                <li class="list-item">24/7 Support</li>
+              </ul>
+            </div>
+            <button class="btn btn-dark">Choose Plan</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!--alert messages start-->
+  <?php echo $alert; ?>
+<!--alert messages end-->
+
+<link href="contact-form.css" rel="stylesheet">
+
+<div class="page-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-7 py-3 mb-5 mb-lg-0">
+        <div class="img-place w-lg-75 wow zoomIn">
+          <img src="../assets/img/illustration_contact.svg" alt="">
+        </div>
+      </div>
+      <div class="col-lg-5 py-3">
+        <h1 class="wow fadeInUp">Need a help? <br>
+        Don't worry just contact us</h1>
+
+         <form id="fcf-form-id" class="fcf-form-class" method="post">
+        
+        <div class="fcf-form-group">
+            <label for="Name" class="fcf-label">Fullname</label>
+            <div class="fcf-input-group">
+                <input type="text" id="Name" name="name" class="fcf-form-control" required>
+            </div>
+        </div>
+
+        <div class="fcf-form-group">
+            <label for="Email" class="fcf-label">Email</label>
+            <div class="fcf-input-group">
+                <input type="email" id="Email" name="email" class="fcf-form-control" required>
+            </div>
+        </div>
+
+        <div class="fcf-form-group">
+            <label for="Phone" class="fcf-label">Phone No.</label>
+            <div class="fcf-input-group">
+                <input type="phone" id="Phone" name="phone" class="fcf-form-control" required>
+            </div>
+        </div>
+
+        <div class="fcf-form-group">
+            <label for="Message" class="fcf-label">Message</label>
+            <div class="fcf-input-group">
+                <textarea id="Message" name="message" class="fcf-form-control" rows="6" maxlength="3000" required></textarea>
+            </div>
+        </div>
+
+        <div class="fcf-form-group">
+            <button type="submit" name="submit" id="fcf-button" class="btn btn-primary">Send Message</button>
+        </div>
+
+    </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="page-footer-section bg-dark fg-white">
+  <div class="container">
+    <div class="row mb-5 py-3">
+      <div class="col-sm-6 col-lg-2 py-3">
+        <h5 class="mb-3">Services</h5>
+        <ul class="menu-link">
+          <li><a href="updates.html" class="">Web Designing And Devlopment</a></li>
+          <li><a href="updates.html" class="">Androide App Devlopment</a></li>
+          <li><a href="updates.html" class="">Software Devlopment</a></li>
+          <li><a href="updates.html" class="">LOGO AND Graphics Designing</a></li>
+          <li><a href="updates.html" class="">SEO & Digital Marketing</a></li>
+        </ul>
+      </div>
+      <div class="col-sm-6 col-lg-2 py-3">
+        <h5 class="mb-3">Company</h5>
+        <ul class="menu-link">
+          <li><a href="about.html" class="">About</a></li>
+          <li><a href="about.html" class="">Team</a></li>
+          <li><a href="contact.php" class="">Careers</a></li>
+          <li><a href="contact.phps class="">HIRING!</a></li>
+        </ul>
+      </div>
+      <div class="col-md-6 col-lg-4 py-3">
+        <h5 class="mb-3">Contact</h5>
+        <ul class="menu-link">
+          <li><a href="contact.php">Contact Us</a></li>
+          <li><a href="https://www.google.com/maps/place/WebPaage+Technologies/@28.3513598,79.4094635,15z/data=!4m5!3m4!1s0x0:0x94776ea26dbe6822!8m2!3d28.3513598!4d79.4094635">503, Civil Lines, Bareilly - 243001 </a></li>
+          <li><a href="mailto:webpaagetech@gmail.com?Subject=Plan%20Info!" class="">webpaagtech@gmail.com</a></li>
+          <li><a href="https://wa.me/919634780846" class=""><span class="mai-logo-whatsapp"></span>&nbsp;+91 9634780846</a></li>
+        </ul>
+      </div>
+      <div class="col-md-6 col-lg-4 py-3">
+        <h5 class="mb-3">Subscribe</h5>
+        <p>Get some offers, news, or updates</p>
+       
+
+        <!-- Social Media Button -->
+        <div class="mt-4">
+          <a href="https://www.facebook.com/webpaage" class="btn btn-fab btn-primary fg-white"><span class="mai-logo-facebook"></span></a>
+          <a href="https://wa.me/919634780846" class="btn btn-fab btn-primary fg-white"><span class="mai-logo-whatsapp"></span></a>
+          <a href="https://www.instagram.com/webpaage/" class="btn btn-fab btn-primary fg-white"><span class="mai-logo-instagram"></span></a>
+          <a href="https://twitter.com/webpaage" class="btn btn-fab btn-primary fg-white"><span class="mai-logo-twitter"></span></a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <hr>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-md-6 py-2">
+        <img src="../assets/img/logo.png" alt="" width="100">
+        <!-- Please don't remove or modify the credits below -->
+        <p class="d-inline-block ml-2">Copyright &copy; <a href="index.php" class="fg-white fw-medium">Webpaage</a>. All rights reserved</p>
+      </div>
+      <div class="col-12 col-md-6 py-2">
+        <ul class="nav justify-content-end">
+          <li class="nav-item"><a href="#" class="nav-link">Terms of Use</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">Privacy Policy</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">Cookie Policy</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
+<script src="../assets/js/jquery-3.5.1.min.js"></script>
+
+<script src="../assets/js/bootstrap.bundle.min.js"></script>
+
+<script src="../assets/vendor/owl-carousel/js/owl.carousel.min.js"></script>
+
+<script src="../assets/vendor/wow/wow.min.js"></script>
+
+<script src="../assets/js/mobster.js"></script>
+
+<script type="text/javascript">
+    if(window.history.replaceState){
+      window.history.replaceState(null, null, window.location.href);
+    }
+</script>
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5f99c2527f0a8e57c2d80199/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+
+<!--Scroll nav color change-->
+<script>
+ $(window).scroll(function(){
+ $('nav').toggleClass('scrolledd', $(this).scrollTop() > 50);
+ });
+</script>
+
+</body>
+</html>
+
+
+
---
+++ html/sendemail.php
@@ -0,0 +1,44 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+
+require_once 'phpmailer/Exception.php';
+require_once 'phpmailer/PHPMailer.php';
+require_once 'phpmailer/SMTP.php';
+
+$mail = new PHPMailer(true);
+
+$alert = '';
+
+if(isset($_POST['submit'])){
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+  $phone = $_POST['phone'];
+
+  try{
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'webpaagetech@gmail.com'; // Gmail address which you want to use as SMTP server
+    $mail->Password = 'Akshat@12345'; // Gmail address Password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = '587';
+
+    $mail->setFrom('webpaagetech@gmail.com'); // Gmail address which you used as SMTP server
+    $mail->addAddress('webpaagetech@gmail.com'); // Email address where you want to receive emails (you can use any of your gmail address including the gmail address which you used as SMTP server)
+
+    $mail->isHTML(true);
+    $mail->Subject = 'Message Received (Contact Page)';
+    $mail->Body = "<h3>Name : $name <br>Email: $email <br>Message : $message <br>Phone : $phone</h3>";
+
+    $mail->send();
+    $alert = '<div class="alert-success">
+                 <span>Message Sent! Thank you for contacting us.</span>
+                </div>';
+  } catch (Exception $e){
+    $alert = '<div class="alert-error">
+                <span>'.$e->getMessage().'</span>
+              </div>';
+  }
+}
+?>
